128a10056aabf15517ccaf33ad44e4006700384c
freshchen <961011595@qq.com>
2019-04-18 15:52:09 +0800

    [fresh] mvc

---
+++ springboot-mvc/src/main/java/com/edu/cn/ecnu/springbootmvc/SpringbootMvcApplication.java
@@ -0,0 +1,13 @@
+package com.edu.cn.ecnu.springbootmvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootMvcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootMvcApplication.class, args);
+    }
+
+}
---
+++ springboot-mvc/src/test/java/com/edu/cn/ecnu/springbootmvc/SpringbootMvcApplicationTests.java
@@ -0,0 +1,16 @@
+package com.edu.cn.ecnu.springbootmvc;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringbootMvcApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
