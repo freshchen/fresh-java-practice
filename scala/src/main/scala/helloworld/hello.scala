dc38db848aac29d9b0ccb4ef6173044c563b9632
freshchen <961011595@qq.com>
2019-07-09 08:57:24 +0800

    [fresh] scala

---
+++ scala/src/main/scala/helloworld/hello.scala
@@ -0,0 +1,14 @@
+package helloworld
+
+/**
+  * @program: fresh-java-practice
+  * @Date: 2019/7/9 1:00
+  * @Author: Ling Chen
+  * @Description:
+  */
+object hello {
+  def main(args: Array[String]): Unit = {
+    println("hello")
+  }
+
+}
