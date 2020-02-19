cc7648b443c5695965d3d0f08bded44bbd2d2eef
freshchen <charles_ling1984@163.com>
2020-02-19 16:08:30 +0800

    [fresh]  test

---
+++ core-java/java11/src/main/java/demo/Test.java
@@ -0,0 +1,13 @@
+package demo;
+
+/**
+ * @author darcy
+ * @since 2020/02/17
+ **/
+public class Test {
+
+    public static void main(String[] args) {
+//        Map.of()
+//        Flow.Publisher
+    }
+}
