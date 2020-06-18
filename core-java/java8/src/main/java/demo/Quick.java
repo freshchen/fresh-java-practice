482f1025fc847dbd21a7b90077aaa573909a4ef7
freshchen <961011595@qq.com>
2020-06-18 17:59:24 +0800

    [fresh] spring cloud security d

--- core-java/java8/src/main/java/demo/Quick.java
+++ core-java/java8/src/main/java/demo/Quick.java
@@ -1,7 +1,6 @@
 package demo;
 
-import java.util.Arrays;
-import java.util.stream.Stream;
+import org.apache.commons.lang3.StringUtils;
 
 /**
  * @author darcy
@@ -10,7 +9,11 @@ import java.util.stream.Stream;
 public class Quick {
     public static void main(String[] args) {
 
-        String s = "12312,123123,12,3,esaf,dsf";
-        Stream.of(",".split(s)).forEach((s1 -> System.out.println(s1)));
+//        String s = "12312,123123,12,3,esaf,dsf";
+//        Stream.of(",".split(s)).forEach((s1 -> System.out.println(s1)));
+        String a = "[asda]";
+        String b = "2";
+        b = StringUtils.isBlank(b) ? a + StringUtils.EMPTY : a + StringUtils.SPACE + b;
+        System.out.println(b);
     }
 }
---
+++ springboot-security/src/main/java/com/example/springbootsecurity/TestController.java
@@ -0,0 +1,38 @@
+package com.example.springbootsecurity;
+
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+/**
+ * @author darcy
+ * @since 2020/06/15
+ **/
+@Controller
+public class TestController {
+
+    @PostMapping({"/", "/home"})
+    public String home() {
+        return "index";
+    }
+
+    @GetMapping("name")
+    @ResponseBody
+    public String name() {
+        UserDetails user = User.withDefaultPasswordEncoder()
+                .username("user")
+                .password("password")
+                .roles("USER")
+                .build();
+        System.out.println(user.getPassword());
+        return "name";
+    }
+
+    @GetMapping("/login")
+    public String login() {
+        return "mylogin";
+    }
+}
