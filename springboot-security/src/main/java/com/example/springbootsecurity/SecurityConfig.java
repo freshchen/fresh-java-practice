c173b9b8c39b05ea46eb3248b0fb45e1f5af9249
freshchen <charles_ling1984@163.com>
2020-06-28 00:16:43 +0800

    [fresh]  SecurityConfig

--- springboot-security/src/main/java/com/example/springbootsecurity/SecurityConfig.java
+++ springboot-security/src/main/java/com/example/springbootsecurity/SecurityConfig.java
@@ -1,14 +1,14 @@
 package com.example.springbootsecurity;
 
-import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 /**
  * @author darcy
  * @since 2020/06/16
  **/
-@Configuration
+@EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
 
